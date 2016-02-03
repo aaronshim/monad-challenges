cc499ffcdc6accb538e1e89fc250611888011021
Aaron Shim <aaron.shim@yale.edu>
2016-02-03 10:34:37 -0500

    First exercise

---
+++ Set 1/RandomNumberGeneration.hs
@@ -0,0 +1,11 @@
+import MCPrelude
+
+fiveRands :: [Integer]
+fiveRands =
+	let randIter (n, seed) = rand seed
+	    firstSeed = (rand . mkSeed) 1
+	in
+	map fst $ take 5 $ iterate randIter firstSeed
+
+main = do
+	print $ foldl (*) 1 fiveRands

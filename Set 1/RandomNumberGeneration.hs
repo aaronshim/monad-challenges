1e3787514c64da28b49471c3c5cf779e65e3d0d5
Aaron Shim <aaron.shim@yale.edu>
2016-02-03 10:41:10 -0500

    formatting for first exercise

--- Set 1/RandomNumberGeneration.hs
+++ Set 1/RandomNumberGeneration.hs
@@ -1,11 +1,10 @@
 import MCPrelude
 
 fiveRands :: [Integer]
-fiveRands =
-	let randIter (n, seed) = rand seed
-	    firstSeed = (rand . mkSeed) 1
-	in
-	map fst $ take 5 $ iterate randIter firstSeed
+fiveRands
+  = let randIter (n, seed) = rand seed
+        firstSeed = (rand . mkSeed) 1
+    in map fst $ take 5 $ iterate randIter firstSeed
 
 main = do
-	print $ foldl (*) 1 fiveRands
+    print $ product fiveRands

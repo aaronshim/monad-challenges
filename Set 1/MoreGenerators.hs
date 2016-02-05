ead4dc49f5b46a85079ce4b07c71b31902f6f26c
Aaron Shim <aaron.shim@yale.edu>
2016-02-05 13:52:22 -0500

    trying to generalize mapping functions to inside duples

--- Set 1/MoreGenerators.hs
+++ Set 1/MoreGenerators.hs
@@ -2,16 +2,19 @@ import MCPrelude
 
 type Gen a = Seed -> (a, Seed)
 
+fOnRand :: (a -> b) -> Gen a -> Gen b
+--fOnRand f g = (\x -> (f . fst $ g x, snd $ g x))
+fOnRand f g = mapT (f . fst . g , snd . g)
+    where mapT (rf, sf) x = (rf x, sf x)
+    -- what's the easiest way to uncurry to get x into both functions
+
 randEven :: Gen Integer -- the output of rand * 2
-randEven s = (2 * r, ns)
-	where (r, ns) = rand s
+randEven = fOnRand (* 2) rand
 randOdd :: Gen Integer -- the output of rand * 2 + 1
-randOdd s = (r + 1, ns)
-	where (r, ns) = randEven s
+randOdd = fOnRand (+ 1) randEven
 randTen :: Gen Integer -- the output of rand * 10
-randTen s = (r * 5, ns)
-	where (r, ns) = randEven s
+randTen = fOnRand (* 5) randEven
 
 main = do
-	let s1 = mkSeed 1
-	print $ product $ map fst [randEven s1, randOdd s1, randTen s1]
+    let s1 = mkSeed 1
+    print $ product $ map fst [randEven s1, randOdd s1, randTen s1]

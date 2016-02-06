aba7013e5d366bd56b276a1ed523a8e023ebbbe1
Aaron Shim <aaron.shim@yale.edu>
2016-02-06 10:50:28 -0500

    Cleaning up some typing stuff

--- Set 1/MoreGenerators.hs
+++ Set 1/MoreGenerators.hs
@@ -4,10 +4,7 @@ import MCPrelude
 type Gen a = Seed -> (a, Seed)
 
 fOnRand :: (a -> b) -> Gen a -> Gen b
---fOnRand f g = (\x -> (f . fst $ g x, snd $ g x))
-fOnRand f g = mapT (f . fst . g , snd . g)
-    where mapT (rf, sf) x = (rf x, sf x)
-    -- what's the easiest way to uncurry to get x into both functions
+fOnRand f g s = (f . fst . g $ s, snd . g $ s)
 
 randEven :: Gen Integer -- the output of rand * 2
 randEven = fOnRand (* 2) rand

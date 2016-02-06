a6e2a49117b4afdedb3df96a9ab714ce40f10571
Aaron Shim <aaron.shim@yale.edu>
2016-02-06 11:21:20 -0500

    currying away the seed (extra parenthesis)

--- Set 1/GeneralizingListsOfGenerators.hs
+++ Set 1/GeneralizingListsOfGenerators.hs
@@ -7,7 +7,7 @@ import GeneralizingRandomPairs
 
 repRandom :: [Gen a] -> Gen [a]
 repRandom [] = \s -> ([], s)
-repRandom (x:xs) = (generalB (:) x $ repRandom xs)
+repRandom (x:xs) = generalB (:) x $ repRandom xs
 
 main = do
 	print $ fst $ repRandom (replicate 3 randLetter) (mkSeed 1)
